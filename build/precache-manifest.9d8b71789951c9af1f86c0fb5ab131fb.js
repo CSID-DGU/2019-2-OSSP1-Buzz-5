9d0a3b0b048cf5cddafada6575dc19131c9df832
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-10-08 14:40:51 +0900

    deploy commit

---
+++ build/precache-manifest.9d8b71789951c9af1f86c0fb5ab131fb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65bf90645a77fad30b27cb7ed0e30661",
+    "url": "/2019-2-OSSP1-Buzz-5/index.html"
+  },
+  {
+    "revision": "ed4c827a8dd644a7c749",
+    "url": "/2019-2-OSSP1-Buzz-5/static/js/2.0dee24b3.chunk.js"
+  },
+  {
+    "revision": "bd14e973cea42233952a",
+    "url": "/2019-2-OSSP1-Buzz-5/static/js/main.1cb0f5ce.chunk.js"
+  },
+  {
+    "revision": "ea7e9550a4c4d5a090c3",
+    "url": "/2019-2-OSSP1-Buzz-5/static/js/runtime-main.e0b658aa.js"
+  }
+]);
