78912346e7506535cbde3089b2b3b135d5254795
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-10-09 19:34:40 +0900

    deploy commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react'
+// import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import store from './store';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Home from './containers/HomePage'
+import Room from './containers/RoomPage'
+import NotFound from './components/NotFound'
+import styles from './App.css'
+
+export class App extends Component {
+    render() {
+        return (
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Switch>
+                        <Route exact path="/" component={Home} />
+                        <Route path="/r/:room" component={Room} />
+                        <Route path="*" component={NotFound} />
+                    </Switch>
+                </BrowserRouter>
+            </Provider>
+        )
+    }
+}
+
+export default App
