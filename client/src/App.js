e90396298ae90b51a026a509609df1a24e1f1472
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-10-21 10:23:32 +0900

    UI commit

--- client/src/App.js
+++ client/src/App.js
@@ -15,15 +15,7 @@ export class App extends Component {
         return (
             <div>
                 <Header/>
-                <Provider store={store} basename={process.env.PUBLIC_URL}>
-                    <HashRouter>
-                        <Switch>
-                            <Route path="/" component={Home}/>
-                            <Route path="/r/:room" component={Room} />
-                            <Route path="*" component={NotFound} />
-                        </Switch>
-                    </HashRouter>
-                </Provider>
+
                 <Footer/>
             </div>
         )
@@ -31,3 +23,13 @@ export class App extends Component {
 }
 
 export default App
+
+{/* <Provider store={store} basename={process.env.PUBLIC_URL}>
+<HashRouter>
+    <Switch>
+        <Route path="/" component={Home}/>
+        <Route path="/r/:room" component={Room} />
+        <Route path="*" component={NotFound} />
+    </Switch>
+</HashRouter>
+</Provider> */}
