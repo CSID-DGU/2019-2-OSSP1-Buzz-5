152eed232af27030bf2a92301ea84e91772d303a
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-10-21 13:10:30 +0900

    Commit

---
+++ client/dist/precache-manifest.6f78d263c6719f320aa1de71dce69aad.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "124f8f53c9e968fd85f12c87c05e5067",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4427dd64e15cca1c43b1",
+    "url": "/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "905c9b3315eee7b92ac8",
+    "url": "/static/css/main.0ee1dc0c.chunk.css"
+  },
+  {
+    "revision": "4427dd64e15cca1c43b1",
+    "url": "/static/js/2.bed30c28.chunk.js"
+  },
+  {
+    "revision": "905c9b3315eee7b92ac8",
+    "url": "/static/js/main.ee876795.chunk.js"
+  },
+  {
+    "revision": "89f2bad89a6f5f59e789",
+    "url": "/static/js/runtime-main.ebfa0bcc.js"
+  },
+  {
+    "revision": "bca0cd3d5393f02359eff04e768224c9",
+    "url": "/static/media/background.bca0cd3d.jpg"
+  },
+  {
+    "revision": "b8496a376070eafbc8695fa508d2867e",
+    "url": "/static/media/banner.b8496a37.jpg"
+  },
+  {
+    "revision": "72004b55909f64a8145cb6d9e60a5e17",
+    "url": "/static/media/bridge.72004b55.png"
+  },
+  {
+    "revision": "2efc5959b425b8fb99ee45f26156d007",
+    "url": "/static/media/color-logo.2efc5959.png"
+  },
+  {
+    "revision": "554a3f5d8884acc60000e9d0efdec7fa",
+    "url": "/static/media/logo_wxw.554a3f5d.png"
+  }
+]);
