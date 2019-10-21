a3b1b6743b6a8fd0810698103166800bfd55a58d
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-10-21 10:17:31 +0900

    Server commit

--- server.js
+++ server.js
@@ -8,14 +8,14 @@ const favicon = require('serve-favicon');
 const compression = require('compression');
 
 const app = express(),
-  options = { 
-    key: fs.readFileSync(__dirname + '/rtc-video-room-key.pem'),
-    cert: fs.readFileSync(__dirname + '/rtc-video-room-cert.pem')
-  },
+  // options = { 
+  //   key: fs.readFileSync(__dirname + '/rtc-video-room-key.pem'),
+  //   cert: fs.readFileSync(__dirname + '/rtc-video-room-cert.pem')
+  // },
   port = process.env.PORT || 3000,
   server = process.env.NODE_ENV === 'production' ?
     http.createServer(app).listen(port) :
-    https.createServer(options, app).listen(port),
+    https.createServer(app).listen(port),
   io = sio(server);
 // compress all requests
 app.use(compression());
