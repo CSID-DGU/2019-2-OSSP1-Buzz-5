aa263caa5c0c8ebd574a9e7905bd9367b421bc08
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-10-27 15:11:54 +0900

    Commit

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,6 @@ import { HashRouter, Switch, Route } from 'react-router-dom';
 import { Provider } from 'react-redux'
 import store from './store';
 import Room from './containers/RoomPage'
-import NotFound from './components/NotFound'
 import Home from './components/Ori_Home';
 import Header from './components/Header';
 import Footer from './components/Footer'
