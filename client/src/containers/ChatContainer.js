4491c8ae3e07485c99921e0e3232ae2901afd43e
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-11-25 11:03:04 +0900

    chat css

--- client/src/containers/ChatContainer.js
+++ client/src/containers/ChatContainer.js
@@ -106,25 +106,25 @@ export class ChatContainer extends Component {
   }
 
   render() {
-    // var messages;
-    // if(this.props.name == window.sessionStorage.getItem('name')) {
-    //    messages = this.state.messages.map(e => (
-    //     <div key={e.key} className="d-flex flex-row-reverse">
-    //       {/* <span className="badge badge-pill badge-primary">{e.name}</span> */}
-    //       <span className="chat_message">{e.msg}</span>
-    //       <span className="chat_timestamp">{e.timestamp}</span>
-    //     </div>
-    //   ))
-    // } else {
-    //   messages = this.state.messages.map(e => (
-    //     <div key={e.key} className="d-flex flex-row">
-    //       <span className="badge badge-pill badge-primary">{e.name}</span>
-    //       <span className="chat_message">{e.msg}</span>
-    //       <span className="chat_timestamp">{e.timestamp}</span>
-    //     </div>
-    //   ))
-    // }
-    const messages = this.renderChat()
+    var messages;
+    if(this.props.name == window.sessionStorage.getItem('name')) {
+       messages = this.state.messages.map(e => (
+        <div key={e.key} className="d-flex flex-row-reverse">
+          {/* <span className="badge badge-pill badge-primary">{e.name}</span> */}
+          <span className="chat_message">{e.msg}</span>
+          <span className="chat_timestamp">{e.timestamp}</span>
+        </div>
+      ))
+    } else {
+      messages = this.state.messages.map(e => (
+        <div key={e.key} className="d-flex flex-row">
+          <span className="badge badge-pill badge-primary">{e.name}</span>
+          <span className="chat_message">{e.msg}</span>
+          <span className="chat_timestamp">{e.timestamp}</span>
+        </div>
+      ))
+    }
+    // const messages = this.renderChat()
     return (
       <div>
         <div className="col">
