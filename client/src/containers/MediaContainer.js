fe25a8051ef9ed1a23cd22bcc4132617519facd4
서진원 <seojin-won@seojin-won-ui-MacBook-Air.local>
2019-11-25 20:58:06 +0900

    screen sharing

--- client/src/containers/MediaContainer.js
+++ client/src/containers/MediaContainer.js
@@ -110,17 +110,17 @@ class MediaBridge extends Component {
   async getDisplay() {
     try {
       console.log(1111111)
-      await navigator.mediaDevices.getDisplayMedia({video:true, audio:true}).then(stream => {
+      await navigator.mediaDevices.getDisplayMedia().then(stream => {
           console.log(22222222)
           stream.oninactive = () => {
-            this.pc.removeStream(this.localStream)  
+            this.pc.removeTrack(this.localStream)  
             this.props.getUserMedia.then(() => {
-              this.pc.addStream(this.localStream)
+              this.pc.addTrack(this.localStream)
             })
         }
         // this.setState({ streamUrl: stream, localStream: stream })
         this.localVideo.srcObject = stream
-        this.pc.addStream(stream)
+        this.pc.addTrack(stream)
       })
     } catch(err) {
       console.log(err)
